3c01ba977463a63a72b1a0eb58c17d4eae8422e2
ian3314273 <ian3314273@gmail.com>
2024-06-04 18:00:50 +0800

    Initial commit

---
+++ javascripts/script.js
@@ -0,0 +1,14 @@
+onsole.log('script loaded')
+function to_loginoption() {
+    window.location.href = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=151&ct=1715082810&rver=7.3.6960.0&wp=MBI_SSL&wreply=https%3a%2f%2fwww.microsoft.com%2frpsauth%2fv1%2faccount%2fSignInCallback%3fstate%3deyJSdSI6Imh0dHBzOi8vd3d3Lm1pY3Jvc29mdC5jb20vemg-tdHciLCJMYyI6IjEwMjgiLCJIb3N0Ijoid3d3Lm1pY3Jvc29mdC5jb20ifQ&lc=1028&id=74335&aadredir=0";
+}
+
+function to_service() {
+    window.location.href = "https://www.microsoft.com/zh-tw/servicesagreement/";
+}
+
+function to_privacy() {
+    window.location.href = "https://privacy.microsoft.com/zh-tw/privacystatement";
+}
+
+
